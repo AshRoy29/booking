8f30e1ba699c3d6ec4faa474b7013b28da15760c
AshRoy29 <ash.rox2920@gmail.com>
2021-11-01 03:16:55 +0600

    Initial commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/AshRoy29/booking
+
+go 1.16
+
+require (
+	github.com/alexedwards/scs/v2 v2.4.0
+	github.com/go-chi/chi/v5 v5.0.5
+	github.com/justinas/nosurf v1.1.1
+)
---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,49 @@
+package handlers
+
+import (
+	"github.com/AshRoy29/booking/pkg/config"
+	"github.com/AshRoy29/booking/pkg/models"
+	"github.com/AshRoy29/booking/pkg/render"
+	"net/http"
+)
+
+
+// Repo is the repository used by the handlers
+var Repo *Repository
+
+// Repository is the type repository
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository {
+		App : a,
+	}
+}
+
+// NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request){
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+	render.RenderTemplates(w, "home.page.tmpl", &models.TemplateData{})
+}
+
+func (m *Repository) About(w http.ResponseWriter, r *http.Request){
+
+	stringMap := make(map[string]string)
+	stringMap["test"] = "OLA"
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
+
+	render.RenderTemplates(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
