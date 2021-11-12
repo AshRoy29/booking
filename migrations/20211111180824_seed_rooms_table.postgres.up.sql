48447381459fe6e06000fd6ca7a8c01a3cb7f728
AshRoy29 <ash.rox2920@gmail.com>
2021-11-13 01:09:19 +0600

    Seeding data into database table

---
+++ migrations/20211111180824_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+    ('Suite','2001-01-01 00:00:00.000','2001-01-01 00:00:00.000'),
+    ('Double Rooms','2001-01-01 00:00:00.000','2001-01-01 00:00:00.000');
---
+++ migrations/20211111181428_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restriction_name,created_at,updated_at) VALUES
+    ('Reservation','2000-01-01 00:00:00.000','2000-01-01 00:00:00.000'),
+    ('Owner Block','2000-01-01 00:00:00.000','2000-01-01 00:00:00.000');
